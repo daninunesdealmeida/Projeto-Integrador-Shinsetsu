a48e0e6617499db3e24e69224627294f7eefaacb
Daniel Nunes de Almeida <daninunesdealmeida@gmail.com>
2021-08-29 12:35:58 -0300

    Initial Commit

---
+++ Shinsetsu/resources/views/produtos.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Produtos</title>
+    </head>
+    <body>
+        <h1>Produto: {{$nome}}</h1>
+        Esta é a view para apresentar os dados do produto.
+    </body>
+</html>
+
+
